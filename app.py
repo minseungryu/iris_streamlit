1780e2156352d2ad24c499f8e13c1144b35623b6
류민승 <ryuminseung96@gmail.com>
2023-09-01 10:15:22 +0900

    update

--- app.py
+++ app.py
@@ -3,21 +3,29 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as ex
-import plotly.graph_objects as go
-import scikit-learn as sklearn
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# import plotly.express as ex
+# import plotly.graph_objects as go
+# import scikit-learn as sklearn
 
 def main():
-    st.markdown("# Hello Streamlit!")
-    st.write(np.__version__)
-    st.write(pd.__version__)
-    st.write(sns.__version__)
-    st.write(plt.__version__)
-    st.write(ex.__version__)
-    st.write(go.__version__)
-    st.write(sklearn.__version__)
 
+    st.markdown("# Hello Streamlit!")
+
+    menu = ['Home', '탐색적 자료 분석', '머신러닝', 'About']
+    choice = st.sidebar.selectbox('메뉴', menu)
+
+    if choice == 'Home':
+        st.subHeader('Home')
+    elif choice == '탐색적 자료 분석':
+        st.subHeader('탐색적 자료 분석')
+    elif choice == '머신러닝':
+        st.subHeader('머신러닝')
+    elif choice == 'About':
+        st.subHeader('About')
+    else:
+        pass
+    
 if __name__ == "__main__":
     main()
