c50cbeba7d45b436656361855f2897e4ab665974
류민승 <ryuminseung96@gmail.com>
2023-09-01 09:53:25 +0900

    update

--- app.py
+++ app.py
@@ -2,10 +2,22 @@
 
 import streamlit as st
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as ex
+import plotly.graph_objects as go
+import scikit-learn as sklearn
 
 def main():
     st.markdown("# Hello Streamlit!")
     st.write(np.__version__)
+    st.write(pd.__version__)
+    st.write(sns.__version__)
+    st.write(plt.__version__)
+    st.write(ex.__version__)
+    st.write(go.__version__)
+    st.write(sklearn.__version__)
 
 if __name__ == "__main__":
     main()
