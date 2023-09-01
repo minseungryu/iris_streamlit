ff4de5401efdcd6b7cdd925587f7efcff491d7e2
류민승 <ryuminseung96@gmail.com>
2023-09-01 09:22:40 +0900

    update

---
+++ app.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+import streamlit as st
+import numpy as np
+
+def main():
+    st.markdown("# Hello Streamlit!")
+    st.write(np.__version__)
+
+if __name__ == "__main__":
+    main()
