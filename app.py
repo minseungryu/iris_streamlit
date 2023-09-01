04579ce29e7b3d787441cc1f87618179dc01ea2b
류민승 <ryuminseung96@gmail.com>
2023-09-01 10:38:11 +0900

    update

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 # -*- coding:utf-8 -*-
 
 import streamlit as st
-import numpy as np
-import pandas as pd
+from eda_app import run_eda_app
 
 
 def main():
 
-    st.markdown("Hello World")
+    st.markdown("# Hello World")
     menu = ["Home", "탐색적 자료 분석", "머신러닝", "About"]
     choice = st.sidebar.selectbox("메뉴", menu)
 
     if choice == "Home":
         st.subheader("Home")
     elif choice == "탐색적 자료 분석":
-        st.subheader("탐색적 자료 분석")
+        #st.subheader("탐색적 자료 분석")
+        run_eda_app()
     elif choice == "머신러닝":
         st.subheader("머신러닝")
     elif choice == "About":
